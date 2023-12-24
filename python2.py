1f55d6eb365077185882369eb855c0f7c7db7cca
Bunny3363 <kbhaskar36@gmail.com>
2023-12-24 21:44:13 +0530

    added coments for taging for releasse and some games links on git

--- python2.py
+++ python2.py
@@ -7,6 +7,7 @@ print("This command gonna be stshed")
 """git branching i useful when we have to add multiople features to be added to the code and we can ditribut them to team to make the work done in parallel to so that as a lead you can merge them to main branch."""
 """publish master branch and experimentation is done in other branches and once this enhansements are good we merge them with main branch for production."""
 """while creating branchs its better to follow prefix standards feature/docs, fix/sum_fn etc"""
+"""tags helps us to make the tag the checkouts as v1.0.0 which can be used like semantic versioning and can be used as release in the code for multiple releases in the porject like python packages -> taging schemes """
 
 """Play git branching game with visualizing git https://git-school.github.io/visualizing-git/"""
 """play game in learngitbanching.js.org"""
