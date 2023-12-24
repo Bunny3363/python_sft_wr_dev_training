614d09cb7a9a776b5346decb4213e6668e83ac1c
Bunny3363 <kbhaskar36@gmail.com>
2023-12-24 20:43:04 +0530

    branching

--- python2.py
+++ python2.py
@@ -1,3 +1,9 @@
 import os
 
 print(os.getcwd())
+
+print("This command gonna be stshed")
+
+"""git branching i useful when we have to add multiople features to be added to the code and we can ditribut them to team to make the work done in parallel to so that as a lead you can merge them to main branch."""
+"""publish master branch and experimentation is done in other branches and once this enhansements are good we merge them with main branch for production."""
+"""while creating branchs its better to follow prefix standards feature/docs, fix/sum_fn etc"""
